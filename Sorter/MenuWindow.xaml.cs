12ddbbec7f5640c2f13051b9f0c1fa52e06abaca
MaxHevchuk <maxhevchuk@gmail.com>
2021-04-06 11:07:48 +0300

    added button to change application culture and click event to it

--- Sorter/MenuWindow.xaml.cs
+++ Sorter/MenuWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,15 +22,30 @@ namespace Sorter
     /// </summary>
     public partial class MenuWindow : Window
     {
+        public MenuWindow()
+        {
+            InitializeComponent();
+        }
+
         public MenuWindow(String culture)
         {
             System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(culture);
             InitializeComponent();
         }
 
-        public void Click(object sender, RoutedEventArgs e)
+        private void ChangeLanguage(object sender, RoutedEventArgs e)
         {
-            MenuWindow menuWindow = new MenuWindow("en");
+            MenuWindow menuWindow;
+            CultureInfo currentCulture = Thread.CurrentThread.CurrentUICulture;
+            if (currentCulture.TwoLetterISOLanguageName == "uk")
+            {
+                menuWindow = new MenuWindow("en");
+            }
+            else
+            {
+                menuWindow = new MenuWindow("uk");
+            }
+
             menuWindow.Show();
             Close();
         }
