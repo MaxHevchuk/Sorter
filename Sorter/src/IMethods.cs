4360d64b0bd7c7e859dece21db98bef1f42fab50
MaxHevchuk <maxhevchuk@gmail.com>
2021-04-21 14:51:45 +0300

    changed type of the input parameters

--- Sorter/src/IMethods.cs
+++ Sorter/src/IMethods.cs
@@ -1,13 +1,15 @@
+using System;
+
 namespace Sorter
 {
     public interface IMethods
     {
-        int BubbleSort(ref int[] array);
-        int CocktailSort(ref int[] array);
-        int InsertionSort(ref int[] array);
-        int MergeSort(ref int[] array);
-        int MergeSort(ref int[] array, int[] temp, int left, int right);
-        int Merge(ref int[] array, int[] temp, int left, int mid, int right);
-        int SelectionSort(ref int[] array);
+        int BubbleSort<T>(ref T[] array, out long time) where T : IComparable<T>;
+        int CocktailSort<T>(ref T[] array, out long time) where T : IComparable<T>;
+        int InsertionSort<T>(ref T[] array, out long time) where T : IComparable<T>;
+        int MergeSort<T>(ref T[] array, out long time) where T : IComparable<T>;
+        int MergeSort<T>(ref T[] array, T[] temp, int left, int right) where T : IComparable<T>;
+        int Merge<T>(ref T[] array, T[] temp, int left, int mid, int right) where T : IComparable<T>;
+        int SelectionSort<T>(ref T[] array, out long time) where T : IComparable<T>;
     }
 }
