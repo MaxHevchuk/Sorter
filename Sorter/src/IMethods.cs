878a807dc98a92db006808b0bd16a1689531f225
MaxHevchuk <maxhevchuk@gmail.com>
2021-05-02 20:30:49 +0300

    change the signature of sorting methods

--- Sorter/src/IMethods.cs
+++ Sorter/src/IMethods.cs
@@ -4,12 +4,12 @@ namespace Sorter
 {
     public interface IMethods
     {
-        int BubbleSort<T>(ref T[] array, out long time) where T : IComparable<T>;
-        int CocktailSort<T>(ref T[] array, out long time) where T : IComparable<T>;
-        int InsertionSort<T>(ref T[] array, out long time) where T : IComparable<T>;
-        int MergeSort<T>(ref T[] array, out long time) where T : IComparable<T>;
+        void BubbleSort<T>(ref T[] array, out long time, out int permutations) where T : IComparable<T>;
+        void CocktailSort<T>(ref T[] array, out long time, out int permutations) where T : IComparable<T>;
+        void InsertionSort<T>(ref T[] array, out long time, out int permutations) where T : IComparable<T>;
+        void MergeSort<T>(ref T[] array, out long time, out int permutations) where T : IComparable<T>;
         int MergeSort<T>(ref T[] array, T[] temp, int left, int right) where T : IComparable<T>;
         int Merge<T>(ref T[] array, T[] temp, int left, int mid, int right) where T : IComparable<T>;
-        int SelectionSort<T>(ref T[] array, out long time) where T : IComparable<T>;
+        void SelectionSort<T>(ref T[] array, out long time, out int permutations) where T : IComparable<T>;
     }
 }
