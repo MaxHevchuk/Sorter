eb71334836103a33a958598a82cec8e294109850
MaxHevchuk <maxhevchuk@gmail.com>
2021-05-03 14:08:35 +0300

    add method for converting DataType.Length

--- Sorter/src/MyConvert.cs
+++ Sorter/src/MyConvert.cs
@@ -21,6 +21,7 @@ namespace Sorter
                 DataType.NumberBinary => StringToInt(strings, 2),
                 DataType.NumberDecimal => StringToInt(strings, 10),
                 DataType.NumberHexadecimal => StringToInt(strings, 16),
+                DataType.Length => strings.Select(word => word.Length).ToArray(),
                 _ => null
             };
         }
@@ -39,8 +40,25 @@ namespace Sorter
                 DataType.NumberBinary => IntToString(data, separator, 2),
                 DataType.NumberDecimal => IntToString(data, separator, 10),
                 DataType.NumberHexadecimal => IntToString(data, separator, 16),
+                DataType.Length => SizeToString(data, separator),
                 _ => null
             };
+        }
+
+        private static string SizeToString(int[] numbers, string separator)
+        {
+            var i = 0;
+            var strings = MenuWindow.TempArray;
+            var array = new string[strings.Length];
+            foreach (var number in numbers)
+            {
+                var word = Array.Find(strings, element => element.Length == number);
+                var wordIdx = Array.IndexOf(strings, word);
+                array[i++] = word;
+                strings = strings.Where((_, idx) => idx != wordIdx).ToArray();
+            }
+
+            return string.Join(separator, array);
         }
 
         /// <summary>
@@ -57,10 +75,10 @@ namespace Sorter
         /// </summary>
         /// <param name="data">Int data array.</param>
         /// <param name="separator">Output separator.</param>
-        /// <param name="toBase">Indicates from which number system need to convert the items.</param>
+        /// <param name="fromBase">Indicates from which number system need to convert the items.</param>
         /// <returns>Returns converted and joined with output separator string array.</returns>
-        private static string IntToString(int[] data, string separator, int toBase) =>
+        private static string IntToString(int[] data, string separator, int fromBase) =>
             string.Join(separator,
-                data.Select(number => Convert.ToString(number, toBase)));
+                data.Select(number => Convert.ToString(number, fromBase)));
     }
 }
